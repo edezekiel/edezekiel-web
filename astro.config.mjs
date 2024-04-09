c2227486f330143df0bfe00bc40ef381648336b8
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2024-04-09 13:57:07 -0500

    feat(): Add icon library

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import sitemap from '@astrojs/sitemap';
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://edezekiel.com',
-	integrations: [sitemap()],
+  site: 'https://edezekiel.com',
+  integrations: [sitemap(), icon({
+		include: {
+			mdi: ['github', 'linkedin', 'twitter']
+		}
+	})]
 });
